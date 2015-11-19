3f201ad16d8c623a6a458c256e23003ac4847bf3
Thiago Scalone <thiago@scalone.com.br>
2015-11-19 12:19:43 -0200

    add more debugs and change from GET to POST on the HTTP request.

--- lib/main.rb
+++ lib/main.rb
@@ -3,13 +3,16 @@ require 'simplehttp'
 class Main < Device
   def self.call
     puts "Hello CloudWalk!"
+    puts "Press to Perform HTTP request"
     getc
     Device::Network.socket
     http = SimpleHttp.new("http", "http-echo.cloudwalk.io")
     http.socket = Device::Network.socket.call
     puts http.socket
-    puts response = http.request("GET", "/", {"Body" => "TESSST"})
-    puts response.body
+    puts response = http.request("POST", "/", {"Body" => "TESSST"})
+    puts "Body #{response.body}"
+    puts "Code #{response.code}"
+    getc
     true
   end
 

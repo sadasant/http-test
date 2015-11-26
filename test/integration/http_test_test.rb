babb3b107c723b5ba09c25f18490aca20197739c
Thiago Scalone <thiago@scalone.com.br>
2015-11-26 15:56:39 -0200

    Add integration test.

---
+++ test/integration/http_test_test.rb
@@ -0,0 +1,8 @@
+class IntegrationHttpTestTest < DaFunk::Test.case
+  def test_perfom_http
+    response = HttpTest.perform
+    assert_equal 200, response.code
+    assert_equal "CLOUDWALK", response.body
+  end
+end
+
--- test/integration/skeleton_test.rb
+++
@@ -1,6 +0,0 @@
-class MainTest < DaFunk::Test.case
-  def test_true
-    assert Main.call
-  end
-end
-

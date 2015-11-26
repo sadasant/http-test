ece6ff0f083a4af14758079bfa1918b30cd0b562
Thiago Scalone <thiago@scalone.com.br>
2015-11-26 13:44:27 -0200

    Refactor Main class.

--- lib/main.rb
+++ lib/main.rb
@@ -2,22 +2,27 @@ require 'simplehttp'
 
 class Main < Device
   def self.call
-    puts "Hello CloudWalk!"
-    puts "Press to Perform HTTP request"
+    puts "HTTP Post test!"
+    puts "Press to perform HTTP request"
     getc
-    Device::Network.socket
-    http = SimpleHttp.new("http", "http-echo.cloudwalk.io")
-    http.socket = Device::Network.socket.call
-    puts http.socket
-    puts response = http.request("POST", "/", {"Body" => "TEST"})
+    response = HttpTest.perform
     puts "Body #{response.body}"
     puts "Code #{response.code}"
+    puts "Press to exit"
     getc
     true
   end
 
   def self.version
     "0.0.1"
+  end
+end
+
+class HttpTest
+  def self.perform
+    http = SimpleHttp.new("http", "http-echo.cloudwalk.io")
+    http.socket = Device::Network.socket.call
+    http.request("POST", "/", {"Body" => "TEST"})
   end
 end
 

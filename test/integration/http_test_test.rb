8f65d9fd257239da9b73549c337e131edb2f3608
Thiago Scalone <thiago@scalone.com.br>
2015-12-02 19:33:01 -0200

    Configure klass(http-test), serial(0000000003) e logical number(003) to test environment.

--- test/integration/http_test_test.rb
+++ test/integration/http_test_test.rb
@@ -1,5 +1,6 @@
 class IntegrationHttpTestTest < DaFunk::Test.case
   def test_perfom_http
+    Device::System.klass = "http-test"
     response = HttpTest.perform
     assert_equal 200, response.code
     assert_equal "CLOUDWALK", response.body
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,11 @@ $LOAD_PATH.unshift "./#{APP_NAME}"
 require 'da_funk'
 
 DaFunk::Test.configure do |t|
-  t.root_path = ROOT_PATH
+  t.root_path      = ROOT_PATH
+  t.logical_number = "003"
+  t.serial         = "0000000003"
 end
+
+Device::Setting.to_staging!
+Device::System.klass = "http-test"
 
